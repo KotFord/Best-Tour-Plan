c5d5d41ed40061e56e7b30a889ae388390c59b1a
KotFord <ryuuzaki__l@mail.ru>
2021-05-19 21:18:34 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -32,3 +32,8 @@ ymaps.ready(init);
         });
     }
 
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+  console.log("Клик по кнопке меню");
+  document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+})
