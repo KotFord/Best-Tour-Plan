ae970bfd58526b0c8e8ca4bb38b263089159600b
KotFord <ryuuzaki__l@mail.ru>
2021-05-21 18:50:04 +0300

    create: modal all

--- js/main.js
+++ js/main.js
@@ -1,42 +1,62 @@
-const hotelSlider = new Swiper('.hotel-slider', {
-  loop: true,
+$(document).ready(function () {
+  const hotelSlider = new Swiper('.hotel-slider', {
+    loop: true,
+  
+    navigation: {
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
+    effect: "coverflow",
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
+  const reviewsSlider = new Swiper('.reviews-slider', {
+    loop: true,
+  
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    },
+  
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
+  ymaps.ready(init);
+      function init(){
+          var myMap = new ymaps.Map("map", {
+              center: [43.57597775, 39.72587285],
+              zoom: 15
+          });
+      }
+  
+  var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", function () {
+    document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+  })
+  menuButton.addEventListener("click", function () {
+    document.querySelector("body").classList.toggle("scroll--mobile--off");
+  });
+  
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+  
+  function openModal() {
+    var targetModal = $(this).attr("data-href");
+    $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
+    $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
+  }
+  function closeModal(e) {
+    e.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
 
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
-  effect: "coverflow",
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
 });
-const reviewsSlider = new Swiper('.reviews-slider', {
-  loop: true,
-
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
-
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
-ymaps.ready(init);
-    function init(){
-        var myMap = new ymaps.Map("map", {
-            center: [43.57597775, 39.72587285],
-            zoom: 15
-        });
-    }
-
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", function () {
-  document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
-})
-menuButton.addEventListener("click", function () {
-  document.querySelector("body").classList.toggle("scroll--mobile--off");
-})
-
